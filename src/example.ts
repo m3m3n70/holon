b8d7878d5fe8f7dc22809fc81ff635a5d8da14d5
Joel Tulloch <joel@jtulloch.com>
2019-10-09 12:20:33 -0400

    update example for provider changes

--- src/example.ts
+++ src/example.ts
@@ -1,29 +1,34 @@
-import { Web3Provider, injectProvider } from "./class/Web3Provider";
-import { Holon } from "./class/Holon";
-import { HolonToken } from "./class/HolonToken";
-import config from "./config/config.json";
+import Web3 from 'Web3';
+import { Holon } from './class/Holon';
+import { HolonClient, ContractProvider } from './class/HolonClient';
+import { HolonToken } from './class/HolonToken';
+import config from './config/config.json';
 
-
-async function getHolon(_holonControllerAddress: string, _provider: Web3Provider): Promise<Holon> {
-    const holon = new Holon(_holonControllerAddress, _provider);
-    await holon.initializeExistingHolon();
-    const holonName = await holon.getHolonName();
-    console.log("Holon Name: " + holonName);
-    return holon;
+async function getHolon(_holonControllerAddress: string, _provider: ContractProvider): Promise<Holon> {
+  const holon = new Holon(_holonControllerAddress, _provider);
+  const holonName = await holon.getName();
+  console.log('Holon Name: ' + holonName);
+  return holon;
 }
 
-async function getHolonToken(_holonTokenAddress: string, _provider: Web3Provider): Promise<HolonToken> {
-    const holonToken = new HolonToken(_holonTokenAddress, _provider);
-    await holonToken.initializeExistingToken();
-    const tokenName = await holonToken.getTokenName();
-    console.log("Token Name: " + tokenName);
-    return holonToken;
+async function getHolonToken(_holonTokenAddress: string, _provider: ContractProvider): Promise<HolonToken> {
+  const holonToken = new HolonToken(_holonTokenAddress, _provider);
+  const tokenName = await holonToken.getName();
+  console.log('Token Name: ' + tokenName);
+  return holonToken;
+}
+
+function injectProvider() {
+  const providerAddress = config.web3host;
+  const httpProvider = new Web3.providers.HttpProvider(providerAddress);
+  return new HolonClient(new Web3(httpProvider));
 }
 
 async function initExample() {
-    const provider = injectProvider();
-    await getHolon(config.holonController, provider);
-    await getHolonToken(config.holonToken, provider);
+  const provider = injectProvider();
+
+  await getHolon(config.holonController, provider);
+  await getHolonToken(config.holonToken, provider);
 }
 
 initExample();
